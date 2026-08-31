f5306a005512908f895f53547f533784b8217bce
Y2o3K <105259175+y2o3k@users.noreply.github.com>
2026-08-31 19:51:34 +0900

    Enhance comments in fcw.py for clarity
    Added example and explanation for relative velocity and TTC.

--- src/fcw.py
+++ src/fcw.py
@@ -1,5 +1,6 @@
 
 #range_m : 전방 객체까지의 거리 
+#ex)내 차 속도 30 m/s, 앞 차 10m/s ,상대속도(velocity) :-20m/s (내 차가 앞차 보다 10m/s 더 빠르기 때문에 둘 사이 거리가 초당 10m 씩 줄어든다.)
 #relative_velocity_mps < 0 : 접근중 
 #relative_velocity_mps >= 0 : 충돌 방향으로 접근하지 않으므로 TTC는 계산하지 않음. 
 #접근 중이면 TTC = range / closing speed 
